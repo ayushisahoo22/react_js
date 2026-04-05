677e30e9110563cf3666a250cfc4d65e1bcba99c
ayushisahoo22 <ayushiis0912@gmail.com>
2026-04-05 15:58:05 +0530

    Created Vite+React project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Ayushi from "./ayushi"
+
+function App() {
+  return (
+    <>
+      <Ayushi/>
+      <h1>This is my first react</h1>
+    </>
+  );
+}
+
+export default App
---
+++ 01vitereact/src/ayushi.jsx
@@ -0,0 +1,6 @@
+function ayushi(){
+    return(
+        <h3>Hello Ayushi!!</h3>
+    )
+}
+export default ayushi
