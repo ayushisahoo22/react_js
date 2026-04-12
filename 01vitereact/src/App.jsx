6d472f0cccf988bf2972d262ff8e957ad929a32a
ayushisahoo22 <ayushiis0912@gmail.com>
2026-04-12 16:51:09 +0530

    Variables and expression in react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,13 @@
 import Ayushi from "./ayushi"
+import Variables from "./practice/Variable"
 
 function App() {
   return (
     <>
-      <Ayushi/>
-      <h1>This is my first react</h1>
+      {/* <Ayushi/>
+      <h1>This is my first react</h1> */}
+
+      <Variables/>
     </>
   );
 }
---
+++ 01vitereact/src/practice/Variable.jsx
@@ -0,0 +1,12 @@
+function variables(){
+    const name = "ayushi";
+    const age = 18;
+
+    //expressions are enclosed in {}
+    // return part is enclosed in ()
+    return (
+        <h3>I am {name} of {age} years old {/*this is for comment*/}</h3>
+    )
+}
+
+export default variables;
